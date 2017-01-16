50a8356adb22583b4d344321f6627b690fd2c39e
Daniel-Newman <dn1999@me.com>
2017-01-16 07:01:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var main = function(){
+$('#vs').on('click', function(){
+ 
+  $('.clam').fadeOut(400);
+ $('.row1').delay(400).fadeOut(1000);
+$('.row2').delay(500).fadeOut(900);
+$('.row3').delay(600).fadeOut(800);
+
+});
+
+}
+
+
+
+
+
+$(document).ready(main);
---
+++ info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+   
+<title>PHP!</title>
+<body>
+
+<?php
+$x = 5;
+echo $x;
+echo "Hello";
+?>
+</body>
+
+</html>
