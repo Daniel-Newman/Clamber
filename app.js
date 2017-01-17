ceb728b6229ec1e913cb43af23f9601fca531425
Daniel-Newman <dn1999@me.com>
2017-01-16 16:31:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ $('#vs').on('click', function(){
  $('.row1').delay(400).fadeOut(1000);
 $('.row2').delay(500).fadeOut(900);
 $('.row3').delay(600).fadeOut(800);
+ $('#vs').delay(400).animate({
+ height:'1000px'
+ 
+ });
 
 });
 
