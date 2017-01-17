2a18702600abcf2c58c91214fe33de32fa2fcb34
Daniel-Newman <dn1999@me.com>
2017-01-16 21:47:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var main = function(){
-$('.vs').on('click', function(){
+$('#vs').on('click', function(){
  
   $('.clam').fadeOut(400);
  $('.row1').delay(400).fadeOut(1000);
 $('.row2').delay(500).fadeOut(900);
 $('.row3').delay(600).fadeOut(800);
- $('.vs').delay(400).animate({
+ $('#vs').delay(400).animate({
  height:'1000px'
  
  });
