db281ec4175a95566d47193a25e784a2b85489f1
Daniel-Newman <dn1999@me.com>
2017-01-18 07:26:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -33,18 +33,11 @@ var zip = function (){
   "background-color":"#1f5bb5"
  });
  }
- $('document').delay(4000).ready(zip);
- 
-$('#c2').on('click', function(){
- 
-  $('.clam').fadeOut(400);
- $('.row1').delay(400).fadeOut(1000);
-$('.row2').delay(500).fadeOut(900);
-$('.row3').delay(600).fadeOut(800);
 
-});
+ 
+$('#c2').on('click', function(zip))
 
-}
+
 
 
 
