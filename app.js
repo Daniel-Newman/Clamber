63a2bb8801fb8aca397a26c305eddf8e5da1658c
Daniel-Newman <dn1999@me.com>
2017-01-17 20:09:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var main = function(){
  $('.wraper').css({"filter": "blur(5px)"});
  $('body').append('<div class="locget"><form method="get"><input = type"text">Zip Code</input><input type ="submit" value="Go"></input></form></div>');
- $('locget').css({
+ $('.locget').css({
  "position":"fixed",
   "margin":"auto 0 0 0",
   "width":"100%",
