f4867e74be61560ee99a55c6cbba9f87978fb99b
Daniel-Newman <dn1999@me.com>
2017-01-17 19:39:40 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var main = function(){
+ $('.wraper').blur(8px);
 $('#c2').on('click', function(){
  
   $('.clam').fadeOut(400);
  $('.row1').delay(400).fadeOut(1000);
 $('.row2').delay(500).fadeOut(900);
 $('.row3').delay(600).fadeOut(800);
- $('#c2').delay(400).animate({
- height:'1000px'
- 
- });
 
 });
 
