0c9e70f79ca3edc81291da816433f5b730f6d6ae
Daniel-Newman <dn1999@me.com>
2017-01-19 16:24:07 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ $(function(){
 var zip = function (){
   $('.wraper').css({"filter": "blur(5px) brightness(35%)"});
   $('.wraper .clam').css({"filter": "blur(5px) brightness(35%)"});
- $('body').append('<div class="locget"><a href="99114">Colville</a><a href="99202">Spokane</a><form method="get" action="99101"><div class="back"><select id ="locationselect" placeholder="Select A Country"><option value ="United States">United States</option><option value ="United Kingdoms">United Kingdoms</option><option value ="Canada">Canada</option></select><br/><br/><input type ="submit" value="Go"></input></form></div></div>');
+ $('body').append('<div class="locget"><div class="back"><a href="99114">Colville</a><a href="99202">Spokane</a><!-- ="United States">United States</option><option value ="United Kingdoms">United Kingdoms</option><option value ="Canada">Canada</option></select><br/><br/><input type ="submit" value="Go">--></div></div>');
  $('.locget').css({
  "position":"absolute",
   "margin":"400px 0 0 0",
-  "width":"100%",
-  "margin":"0 auto",
-   "text-align":"center" ,
-  "filter": "drop-shadow(3px 3px 8px #DDD, 8px)",
-    "width":"180px",
-  "padding":"12px 20px",
-"margin":"8px", 
-  "border-radius":"5px",
-"border":"3px solid #347BE6",
-  "background-color":"#99c2ff"
+  "width":"100%"
+   
  });
  $('.locget .back').css({
  "width":"200px",
