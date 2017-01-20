cea00cedd3a7afffd9e1fb1f385c98f5cb3958e2
Daniel-Newman <dn1999@me.com>
2017-01-19 19:35:39 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ $(function(){
 var zip = function (){
   $('.wraper').css({"filter": "blur(5px) brightness(35%)"});
   $('.wraper .clam').css({"filter": "blur(5px) brightness(35%)"});
- $('body').append('<div class="locget"><div class="back"><a href="99114">Colville</a><a href="99202">Spokane</a><!-- ="United States">United States</option><option value ="United Kingdoms">United Kingdoms</option><option value ="Canada">Canada</option></select><br/><br/><input type ="submit" value="Go">--></div></div>');
+ $('body').append('<div class="locget"><div class="backm"><a href="99114">Colville</a><a href="99202">Spokane</a></div></div>');
  $('.locget').css({
  "position":"absolute",
   "margin":"400px 0 0 0",
   "width":"100%"
      
  });
- $('.locget .back').css({
+ $('.locget .backm').css({
  "width":"200px",
   "margin":"0 auto",
    "text-align":"center" ,
@@ -17,10 +17,11 @@ var zip = function (){
    "color":"white",
 "text-decoration": "none" 
  });
- $('.locget .back a').css({
+ $('.locget .backm a').css({
  "width":"180px",
   "padding":"12px 20px",
 "margin":"8px",
+      "color":"white",
    "text-decoration":"none",
   "background-color":"#347BE6"
  });
